68745c7d1d579a0ad167137fbc66794641d40c1c
Michael Smith <mike@noiseparticles.com>
2014-08-21 21:07:49 -0400

    fixing project structure

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
-(defproject app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject clojlike "0.1.0-SNAPSHOT"
+  :description "roguelike written in clojure"
+  :url "http://www.noiseparticles.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clojure-lanterna "0.9.0"]]
+  :main clojlike.core)
