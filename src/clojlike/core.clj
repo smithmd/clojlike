1951da9e796c0fc34bd068ca3b8376cdf19d0611
Michael Smith <mike@noiseparticles.com>
2014-08-21 21:14:01 -0400

    added main, loads a terminal

--- src/clojlike/core.clj
+++ src/clojlike/core.clj
@@ -1,6 +1,18 @@
-(ns app.core)
+(ns clojlike.core
+  (:require [lanterna.screen :as s]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn main [screen-type]
+  (let [screen (s/get-screen screen-type)]
+    (s/in-screen screen
+      (s/put-string screen 0 0 "Welcome to Clojlike!")
+      (s/put-string screen 0 1 "Press any key to exit...")
+      (s/redraw screen)
+      (s/get-key-blocking screen))))
+
+(defn -main [& args]
+  (let [args (set args)
+        screen-type (cond
+                      (args ":swing") :swing
+                      (args ":text")  :text
+                      :else           :auto)]
+    (main screen-type)))
